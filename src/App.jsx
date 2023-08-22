985fe920de962373135fafddb3f655be59f9252f
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-22 12:28:14 +0200

    update board function and turn logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,48 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Square from './components/Square';
 
+const TURNS = ['X', 'O'];
+
 function App() {
   const [board, setBoard] = useState([]);
+  const [turn, setTurn] = useState(0);
 
   useEffect(() => {
-    const initialBoard = Array(8).fill(null);
+    const initialBoard = Array(9).fill(null);
     setBoard(initialBoard);
   }, []);
 
-  console.log(board);
+  const updateBoard = (index) => {
+    if (board[index]) return;
+
+    const newBoard = [...board];
+    newBoard[index] = TURNS[turn % 2];
+    setBoard(newBoard);
+
+    const newTurn = turn + 1;
+    setTurn(newTurn);
+    console.log(newBoard);
+  };
+
   return (
     <>
-      <Square>X</Square>
+      <main className='board'>
+        <h1 className='title'>Tic-tac-toe</h1>
+        <section className='game'>
+          {board.map((square, index) => {
+            return (
+              <Square
+                className='square'
+                updateBoard={updateBoard}
+                key={index}
+                index={index}
+              >
+                {board[index]}
+              </Square>
+            );
+          })}
+        </section>
+      </main>
     </>
   );
 }
