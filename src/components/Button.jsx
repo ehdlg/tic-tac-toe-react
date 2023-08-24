3cc2f3b4a89da4418a2879e65f0e05ecf082425a
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-08-24 18:03:41 +0200

    Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,1 +1,12 @@
-function Button() {}
+function Button({ children, className, onClick = null }) {
+  const handleClick = () => {
+    onClick();
+  };
+  return (
+    <button className={className} onClick={handleClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
